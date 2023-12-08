7d2bf0d33cfcbd99bd33be51caa8ca0fb36642be
lvlivingston <lvlivingston@gmail.com>
2023-12-07 18:34:50 -0600

    completed lesson part 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Define the home view
@@ -20,3 +21,15 @@ def finches_detail(request, finch_id):
     return render(request, 'finches/detail.html', {
         'finch': finch,
     })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['scientificname', 'mass', 'description', 'diet']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
