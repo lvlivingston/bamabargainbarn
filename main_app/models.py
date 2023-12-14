67d7c667de09ae11bd2a9042be1d13a3ec75c754
lvlivingston <lvlivingston@gmail.com>
2023-12-14 16:19:40 -0600

    confirmed sessions & quantity start in model correct

--- main_app/models.py
+++ main_app/models.py
@@ -9,10 +9,13 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=250)
     category = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='static/images/')
+    image = models.ImageField(upload_to='static/images/', blank=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     totalRating = models.IntegerField()
+    is_available = models.BooleanField(default=True)
     inventory = models.PositiveIntegerField(default=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -60,7 +63,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f"{self.quantity} of {self.product.title} in Order {self.order.date}"
